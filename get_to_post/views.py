5f8c2a9f9cfe816aea8d4b778949c260cf79ab50
Ram Rachum <ram@rachum.com>
2012-11-21 01:30:13 +0200

    -

--- get_to_post/views.py
+++ get_to_post/views.py
@@ -9,9 +9,12 @@ class GetToPostView(django.views.generic.TemplateView):
     def get_context_data(self, **kwargs):
         context_data = super(GetToPostView, self).get_context_data(**kwargs)
         
-        post_url = request.GET['url']
+        if 'url' not in self.request.GET:
+            raise django.http.Http404
+        
+        post_url = self.request.GET['url']
         post_data = OrderedDict([(key, value) for key, value in
-                                 request.GET.items() if key != 'url'])
+                                 self.request.GET.items() if key != 'url'])
         
         context_data.update({'post_url': post_url,
                              'post_data': post_data,})
